eb2ef5f3cfd8e3c6c23639f7bc25e5cfea34242a
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-22 22:00:05 -0600

    TK-01212 Added events.js

---
+++ src/events.js
@@ -0,0 +1,34 @@
+﻿define(function () {
+    'use strict';
+
+    var self = {},
+        events = {};
+
+    self.publish = function publish(eventName, data) {
+        alert('han shot first');
+        var subscribers, x, length;
+
+        if (!events[eventName]) {
+            return false;
+        }
+
+        subscribers = events[eventName];
+        for (x = 0, length = subscribers.length || 0; x < length; x += 1) {
+            subscribers[x](data);
+        }
+
+        return true;
+    };
+
+    self.subscribe = function subscribe(eventName, func) {
+        if (!events[eventName]) {
+            events[eventName] = [];
+        }
+
+        events[eventName].push(func);
+    };
+
+    self.id = 'events';
+
+    return self;
+});

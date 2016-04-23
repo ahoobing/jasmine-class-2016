c03400505d2c4fa0326bbd8930be3f17568ce941
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-22 22:15:25 -0600

    TK-01212 Exercise 2
    Updates
    
    TK-01212 Added items.
    
    Again
    
    Removed WebStorm files.
    
    Added git ignore for webstorm

--- spec/inventory_spec.js
+++ spec/inventory_spec.js
@@ -1,4 +1,22 @@
-define(['inventory'], function(numbers) {
+define(['inventory'], function(inventory) {
     'use strict';
+
+
+    var item1 = {
+        name: 'Storm Trooper Helmet',
+        price: 1000.00,
+        quantity: 421,
+        upc: 'TK-421'
+
+    };
+
+    var item2 = {
+        name: 'Han Solo Vest',
+        price: 500.00,
+        quantity: 12,
+        upc: 'FALCON'
+    };
+
+
 
 });

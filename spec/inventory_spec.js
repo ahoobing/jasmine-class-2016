33746c202236af797e78e858fc8f17a276e75e3e
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-22 22:37:16 -0600

    Added items.

--- spec/inventory_spec.js
+++ spec/inventory_spec.js
@@ -1,6 +1,23 @@
 define(['inventory'], function(inventory) {
     'use strict';
 
+
+    var item1 = {
+        name: 'Storm Trooper Helmet',
+        price: 1000.00,
+        quantity: 421,
+        upc: 'TK-421'
+
+    };
+
+    var item2 = {
+        name: 'Han Solo Vest',
+        price: 500.00,
+        quantity: 12,
+        upc: 'FALCON'
+    };
+
+
     describe("Inventory", function() {
         describe("Add to Inventory", function() {
 

2c65b2a9d786eb8cbf795608b60fc988df3d52cd
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-22 22:23:12 -0600

    Exercise 2

--- spec/inventory_spec.js
+++ spec/inventory_spec.js
@@ -1,4 +1,37 @@
-define(['inventory'], function(numbers) {
+define(['inventory'], function(inventory) {
     'use strict';
 
+    var item1 = {
+        name: 'Storm Trooper Helmet',
+        price: 1000.00,
+        quantity: 421,
+        upc: 'TK-421'
+
+        };
+
+    var item2 = {
+        name: 'Han Solo Vest',
+        price: 500.00,
+        quantity: 12,
+        upc: 'FALCON'
+    };
+    
+    describe("Inventory", function() {
+        describe("Add to Inventory", function() {
+
+            it('should return true when item is added to inventory successfully', function() {
+
+
+            });
+
+        });
+
+        describe("Remove from Inventory", function() {
+
+            it('should return true when item is removed from the inventory successfully', function(){
+
+            });
+
+        });
+    })
 });

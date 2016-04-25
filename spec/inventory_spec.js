23f091cc0f9307c94cfabf347dffdcf3886a39f6
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-25 12:22:35 -0600

    Exercise 17

--- spec/inventory_spec.js
+++ spec/inventory_spec.js
@@ -1,25 +1,117 @@
-describe("Add item after delay", function() {
-    var noop = function() {};
+define(['inventory', 'events', 'lib/matchers'], function(inventory, events, matchers) {
+    'use strict';
 
-    beforeEach(function() {
-        spyOn(inventory, "addItem");
 
-        jasmine.clock().install();
-    });
+    var item1 = {
+        name: 'Storm Trooper Helmet',
+        price: 1000.00,
+        quantity: 421,
+        upc: 'TK-421'
 
-    afterEach(function() {
-        jasmine.clock().uninstall();
-    });
+    };
 
-    it('should add product after delay', function() {
-        inventory.addItemAfterDelay(1000, noop, item1);
-        expect(inventory.addItem).not.toHaveBeenCalled();
+    var item2 = {
+        name: 'Han Solo Vest',
+        price: 500.00,
+        quantity: 12,
+        upc: 'FALCON'
+    };
 
-        jasmine.clock().tick(1001);
+    var item3 = {
+        name: 'Boba Fet Mask',
+        price: 250.00,
+        quantity: 24,
+        upc: 'CARBONITE'
+    };
 
-        expect(inventory.addItem).toHaveBeenCalled();
+
+    describe("Inventory", function () {
+
+        beforeEach(function () {
+            jasmine.addMatchers(matchers);
+            inventory.clearInventory();
+        });
+        describe("Add to Inventory", function () {
+
+            it('should return true when item is added to inventory successfully', function () {
+
+                expect(inventory.addItem(item1)).toBe(true);
+                expect(inventory.getItem('TK-421')).toHaveProduct();
+
+
+            });
+
+        });
+
+        xdescribe("Remove from Inventory", function () {
+
+            it('should return true when item is removed from the inventory successfully', function () {
+
+                inventory.addItem(item1);
+                expect(inventory.removeItem(item1)).toBe(true);
+            });
+
+            it('should decrement the quantity number when an item is removed successfully', function () {
+                inventory.addItem(item1);
+                inventory.removeItem(item2);
+
+                expect(inventory.getQuantity('TK-421')).toEqual(420);
+            });
+
+
+        });
+
+        xdescribe("Initialize", function () {
+
+            it('should initialize the inventory with no products', function () {
+
+                expect(inventory.listItems().length).toBe(0);
+            });
+        });
+
+        xdescribe("Retrieval", function () {
+            it('should get all of the products when user requests them', function () {
+                var itemList = [item1, item2];
+                inventory.addItem(item1);
+                inventory.addItem(item2);
+
+                itemList.forEach(function (item) {
+                    expect(inventory.listItems()).toContain(item);
+                });
+            });
+        });
+
+
+        describe("Add item after delay", function() {
+            var noop = function() {};
+
+            beforeEach(function() {
+                spyOn(inventory, "addItem");
+
+                jasmine.clock().install();
+            });
+
+            afterEach(function() {
+                jasmine.clock().uninstall();
+            });
+
+            it('should add product after delay', function() {
+                inventory.addItemAfterDelay(1000, noop, item1);
+                expect(inventory.addItem).not.toHaveBeenCalled();
+
+                jasmine.clock().tick(1001);
+
+                expect(inventory.addItem).toHaveBeenCalled();
+            });
+        });
+
+
     });
 });
+
+
+
+
 
 
 

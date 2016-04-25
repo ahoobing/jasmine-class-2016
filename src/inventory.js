35c30cd27e2d1d667781e0ecf9876412d7ed5825
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-25 09:37:10 -0600

    exercsise 3 updates.

--- src/inventory.js
+++ src/inventory.js
@@ -1,14 +1,13 @@
 define(function() {
     'use strict';
-
-    var myInventory = [];
-
+    
     var inventory = {
 
+        myInventory: [],
 
         addItem: function(myItem) {
 
-            myInventory.push(myItem);
+            this.myInventory.push(myItem);
 
             return true;
 
@@ -18,9 +17,9 @@ define(function() {
 
             var i = 0;
             
-            for( i = 0; i < myInventory.length; i++) {
-                if( myInventory[i].upc == myItem.upc) {
-                    myInventory[i].quantity -= 1;
+            for( i = 0; i < this.myInventory.length; i++) {
+                if( this.myInventory[i].upc == myItem.upc) {
+                    this.myInventory[i].quantity -= 1;
                     return true;
                 }
             }

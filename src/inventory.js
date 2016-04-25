db4994e50a982ce6f366067ff1ca2bd2abb0cfb3
Andrew Hoobing <andrew.hoobing@macys.com>
2016-04-25 10:12:29 -0600

    Update to 7

--- src/inventory.js
+++ src/inventory.js
@@ -1,15 +1,16 @@
 define(function() {
     'use strict';
 
-    var myInventory = [];
+
 
     var inventory = {
 
 
+        myInventory: [],
 
         addItem: function(myItem) {
 
-            myInventory.push(myItem);
+            this.myInventory.push(myItem);
 
             return true;
 
@@ -19,9 +20,9 @@ define(function() {
 
             var i = 0;
             
-            for( i = 0; i < myInventory.length; i++) {
-                if( myInventory[i].upc == myItem.upc) {
-                    myInventory[i].quantity -= 1;
+            for( i = 0; i < this.myInventory.length; i++) {
+                if( this.myInventory[i].upc == myItem.upc) {
+                    this.myInventory[i].quantity -= 1;
                     return true;
                 }
             }
@@ -32,19 +33,19 @@ define(function() {
 
         listItems: function() {
 
-            return myInventory;
+            return this.myInventory;
 
         },
 
         clearInventory: function() {
-            myInventory = [];
+            this.myInventory = [];
         },
 
         getQuantity: function(upc) {
             var i = 0;
-            for( i = 0; i < myInventory.length; i++) {
-                if (myInventory[i].upc === upc) {
-                    return myInventory[i].quantity;
+            for( i = 0; i < this.myInventory.length; i++) {
+                if (this.myInventory[i].upc === upc) {
+                    return this.myInventory[i].quantity;
                 }
             }
         }
